f33a4a7830a9028d07fcd94f4bfaa14bd6358d3a
Bonnie Baumeister <bonnie.hoika@gmx.de>
2023-08-10 11:25:49 +0200

    add first toggle function

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // phew… not a lot going on here. Please add some code!
+const toggleButtonBookmark = document.querySelector(
+  '[data-js="toggle-button"]'
+);
+
+//console.log("test");
+
+toggleButtonBookmark.addEventListener("click", () => {
+  toggleButtonBookmark.classList.toggle("bookmark--active");
+});
+
+//toggleButtonBookmark.addEventListener( , () =>{});
+
+//toggleButtonBookmark.addEventListener( , () =>{});
